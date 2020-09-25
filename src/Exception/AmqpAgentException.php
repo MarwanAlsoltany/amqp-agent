d3f2a88508c257ab5e844edcdb2019b0bd1cfd8e
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2020-09-25 21:21:05 +0200

    Fix issue of wrong FQN * Fix issue of wrong fully-qualified name when casting the class to a string * Change default message of rethrowException method to a more useful one * Add a new parameter to change the wrapping thrown exception class * Rename the method rethrowException to rethrow * Add rethrowException method as an alias for rethrow method

--- src/Exception/AmqpAgentException.php
+++ src/Exception/AmqpAgentException.php
@@ -9,6 +9,7 @@
 namespace MAKS\AmqpAgent\Exception;
 
 use Exception as CoreException;
+use MAKS\AmqpAgent\Helper\Utility;
 
 /**
  * AMQP Agent base exception class.
@@ -33,18 +34,44 @@ class AmqpAgentException extends CoreException
      */
     public function __toString()
     {
-        return __CLASS__ . ": [{$this->code}]: {$this->message} \n";
+        return static::class . ": [{$this->code}]: {$this->message}\n{$this->getTraceAsString()}\n";
     }
 
     /**
      * Rethrows an exception with an additional message.
-     * @param CoreException $exception
-     * @param string $message
+     * @param CoreException $exception The exception to rethrow.
+     * @param string|null $message An additional message to add to the wrapping exception before the message of the passed exception.
+     * @param string|bool $wrap Wether to throw the exception using the passed class (FQN), in the same exception type (true), or wrap it with the class this method was called on (false). Any other value will be translated to false.
      * @return void
      */
-    public static function rethrowException(CoreException $exception, string $message = 'Rethrown Exception!'): void
+    public static function rethrow(CoreException $exception, ?string $message = null, $wrap = false): void
     {
-        $error = get_class($exception);
-        throw new $error($message, (int)$exception->getCode(), $exception);
+        if (null === $message) {
+            $trace = Utility::backtrace(['file', 'line', 'class', 'function']);
+            $prefix = (isset($trace['class']) ? "{$trace['class']}::" : "{$trace['file']}({$trace['line']}): ");
+            $suffix = "{$trace['function']}() failed!";
+            $message = 'Rethrown Exception: ' . $prefix . $suffix . ' ';
+        } else {
+            $message = strlen($message) ? $message . ' ' : $message;
+        }
+
+        $error = $wrap === true
+            ? get_class($exception)
+            : (class_exists($wrap) && is_subclass_of($wrap, 'Exception') ? $wrap : static::class);
+
+        throw new $error($message . (string)$exception->getMessage(), (int)$exception->getCode(), $exception);
+    }
+
+    /**
+     * Rethrows an exception with an additional message.
+     * @deprecated 1.2.0 Use `self::rethrow()` instead.
+     * @param CoreException $exception The exception to rethrow.
+     * @param string|null $message An additional message to add to the wrapping exception before the message of the passed exception.
+     * @param string|bool $wrap Wether to throw the exception using the passed class (FQN), in the same exception type (true), or wrap it with the class this method was called on (false). Any other value will be translated to false.
+     * @return void
+     */
+    public static function rethrowException(): void
+    {
+        static::rethrow(...func_get_args());
     }
 }
