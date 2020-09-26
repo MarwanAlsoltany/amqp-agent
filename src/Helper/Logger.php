MarwanAlsoltany/amqp-agent

commit ba308fc834160a9d27e72e1bb43ff7f4645d9111

Update logger class * Refactor fallback path guessing when no path is specified.

src/Helper/Logger.php
@@ -129,8 +129,9 @@ class Logger
         }
 
         if (null === $directory) {
-            $fallback = dirname(Utility::backtrace('file'));
-            $directory = strlen($_SERVER["DOCUMENT_ROOT"]) ? $_SERVER["DOCUMENT_ROOT"] : $fallback;
+            $fallback = $_SERVER["DOCUMENT_ROOT"];
+            $backtrace = Utility::backtrace(['file']);
+            $directory = strlen($fallback) ? $fallback : (isset($backtrace['file']) ? dirname($backtrace['file']) : __DIR__);
             Utility::emit(
                 [
                     'yellow' => sprintf('%s() was called without specifying a directory.', __METHOD__),
@@ -146,7 +147,7 @@ class Logger
 
         // create log file if it does not exist
         if (!is_file($file) && is_writable($directory)) {
-            $signature = 'Created by ' . __METHOD__ . date('() \o\\n l jS \of F Y h:i:s A (Ymdhis)') . PHP_EOL;
+            $signature = 'Created by ' . __METHOD__ . date('() \on l jS \of F Y h:i:s A (Ymdhis)') . PHP_EOL;
             file_put_contents($file, $signature, null, stream_context_create());
             chmod($file, 0775);
         }

commit 082fd7a0a78c4aa69b52a1dbbc30e823d454954b

Update logger class * Refactor fallback path guessing when no path is specified. * Fix an issue introduced by mistake in ba308fc834160a9d27e72e1bb43ff7f4645d9111

src/Helper/Logger.php
@@ -129,9 +129,10 @@ class Logger
         }
 
         if (null === $directory) {
-            $fallback = $_SERVER["DOCUMENT_ROOT"];
             $backtrace = Utility::backtrace(['file']);
-            $directory = strlen($fallback) ? $fallback : (isset($backtrace['file']) ? dirname($backtrace['file']) : __DIR__);
+            $fallback1 = strlen($_SERVER["DOCUMENT_ROOT"]) ? $_SERVER["DOCUMENT_ROOT"] : null;
+            $fallback2 = isset($backtrace['file']) ? dirname($backtrace['file']) : __DIR__;
+            $directory = $fallback1 ?? $fallback2;
             Utility::emit(
                 [
                     'yellow' => sprintf('%s() was called without specifying a directory.', __METHOD__),
@@ -147,7 +148,7 @@ class Logger
 
         // create log file if it does not exist
         if (!is_file($file) && is_writable($directory)) {
-            $signature = 'Created by ' . __METHOD__ . date('() \on l jS \of F Y h:i:s A (Ymdhis)') . PHP_EOL;
+            $signature = 'Created by ' . __METHOD__ . date('() \o\\n l jS \of F Y h:i:s A (Ymdhis)') . PHP_EOL;
             file_put_contents($file, $signature, null, stream_context_create());
             chmod($file, 0775);
         }