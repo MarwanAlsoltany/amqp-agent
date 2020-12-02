c11727e54c2fadf395f5c7e2c13d4b131916f54c
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2020-12-02 23:50:34 +0100

    Add RPC enpoints' interfaces * Add AbstractEndpointInterface.php * Add ClientEndpointInterface.php * Add ServerEndpointInterface.php

---
+++ src/RPC/ClientEndpointInterface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2020
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace MAKS\AmqpAgent\RPC;
+
+use MAKS\AmqpAgent\RPC\AbstractEndpointInterface;
+
+/**
+ * An interface defining the basic methods of a client.
+ * @since 2.0.0
+ */
+interface ClientEndpointInterface extends AbstractEndpointInterface
+{
+    /**
+     * Sends the passed request to the server using the passed queue.
+     * @param string|AMQPMessage $request The request body or an `AMQPMessage` instance.
+     * @param string|null $queueName [optional] The name of queue to send through.
+     * @return string The response body.
+     */
+    public function request($request, ?string $queueName = null): string;
+
+    /**
+     * Sends the passed request to the server using the passed queue.
+     * Alias for `self::request()`.
+     * @param string|AMQPMessage $request The request body or an `AMQPMessage` instance.
+     * @param string|null $queueName [optional] The name of queue to send through.
+     * @return string The response body.
+     */
+    public function call($request, ?string $queueName = null): string;
+}
---
+++ src/RPC/ServerEndpointInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2020
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace MAKS\AmqpAgent\RPC;
+
+use MAKS\AmqpAgent\RPC\AbstractEndpointInterface;
+
+/**
+ * An interface defining the basic methods of a server.
+ * @since 2.0.0
+ */
+interface ServerEndpointInterface extends AbstractEndpointInterface
+{
+    /**
+     * Listens on requests coming via the passed queue and processes them with the passed callback.
+     * Alias for `self::respond()`.
+     * @param callable|null $callback [optional] The callback to process the request. This callback will be passed an `AMQPMessage` and must return a string.
+     * @param string|null $queueName [optional] The name of the queue to listen on.
+     * @return string The last processed request.
+     */
+    public function respond(?callable $callback = null, ?string $queueName = null): string;
+
+    /**
+     * Listens on requests coming via the passed queue and processes them with the passed callback.
+     * Alias for `self::respond()`.
+     * @param callable|null $callback [optional] The callback to process the request. This callback will be passed an `AMQPMessage` and must return a string.
+     * @param string|null $queueName [optional] The name of the queue to listen on.
+     * @return string The last processed request.
+     */
+    public function serve(?callable $callback = null, ?string $queueName = null): string;
+}
