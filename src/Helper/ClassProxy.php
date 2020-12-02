b8fcd3a56fc7b3672d6ddc539d79d11d14bbdc6d
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2020-12-02 23:39:34 +0100

    Add class proxy class * Add ClassProxy.php * Add ClassProxyTrait.php

---
+++ src/Helper/ClassProxy.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2020
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace MAKS\AmqpAgent\Helper;
+
+/**
+ * A class containing methods for proxy methods calling, properties manipulation, and class utilities.
+ *
+ * Call example:
+ * ```
+ * ClassProxy::call($object, 'someMethod', $arguments);
+ * ```
+ * Get example:
+ * ```
+ * ClassProxy::get($object, 'someProperty');
+ * ```
+ * Set example:
+ * ```
+ * ClassProxy::set($object, 'someProperty', $newValue);
+ * ```
+ *
+ * @since 2.0.0
+ */
+class ClassProxy
+{
+    use ClassProxyTrait {
+        callMethod as call;
+        setProperty as set;
+        getProperty as get;
+        castObjectToClass as cast;
+    }
+}
