ae3db59aab3e8d28aac48bd5a4eceeda2e000b0d
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-01-12 05:29:16 +0100

    Update singleton class * Fix coding style issue

--- src/Helper/Singleton.php
+++ src/Helper/Singleton.php
@@ -67,7 +67,7 @@ abstract class Singleton
     {
         $subclass = static::class;
         if (!isset(self::$instances[$subclass])) {
-            self::$instances[$subclass] = new static;
+            self::$instances[$subclass] = new static();
         }
         return self::$instances[$subclass];
     }
